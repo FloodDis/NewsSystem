2b5d00ec8ed467329d1c799ccadc9ef4a5a540fc
FloodDis <vlad.sechenov@mail.ru>
2022-02-12 17:35:25 +0700

    Написал прототип метода GetTypeOfArticle

--- Новостная система(Стажировка)/Article.h
+++ Новостная система(Стажировка)/Article.h
@@ -22,5 +22,6 @@ public:
 	void SetID(int newID);
 	string GetHeadLine();
 	void SetHeadLine(string newHeadLine);
+	TypeOfArticle GetTypeOfArticle();
 	void SetTypeOfArticle(TypeOfArticle newType);
 };
