8027e57f4df99b6d91a147e03a002348b7f64805
FloodDis <vlad.sechenov@mail.ru>
2022-02-12 19:48:15 +0700

    Написал прототип метода AddArticle класса NewsSystem

--- Новостная система(Стажировка)/NewsSystem.h
+++ Новостная система(Стажировка)/NewsSystem.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Article.h"
+#include "RecommendationSystem.h"
+
+class NewsSystem
+{
+private:
+	list<Article> _articles;
+	map<int, int> _popularity;
+	RecommendationSystem _recommendationSystem;
+public:
+	void AddArticle(Article newArticle);
+};
