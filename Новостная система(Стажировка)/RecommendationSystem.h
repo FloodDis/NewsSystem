5c7c4c43480a49f1e24649b23954b7ea7f36201c
FloodDis <vlad.sechenov@mail.ru>
2022-02-12 19:10:00 +0700

    Написал прототип конструктора класса RecommendationSystem

--- Новостная система(Стажировка)/RecommendationSystem.h
+++ Новостная система(Стажировка)/RecommendationSystem.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <map>
+#include "Article.h"
+#include <list>
+using namespace std;
+
+class RecommendationSystem
+{
+private:
+	map<string, list<Article>> _redList;
+public:
+	RecommendationSystem();
+
+};
