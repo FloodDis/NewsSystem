ba27c311a7b67a925c0b4b508e340ac24d79dffe
FloodDis <vlad.sechenov@mail.ru>
2022-02-26 02:30:27 +0700

    Написал реализацию метода DeleteArticle класса NewsSystem

--- Новостная система(Стажировка)/NewsSystem.cpp
+++ Новостная система(Стажировка)/NewsSystem.cpp
@@ -10,3 +10,20 @@ void NewsSystem::AddArticle(Article newArticle)
 	_articles.push_back(newArticle);
 	_popularity.insert({ newArticle.GetID(),0 });
 }
+
+void NewsSystem::DeleteArticle(Article articleToDelete)
+{
+	list<Article> ::iterator it;
+	int idToDelete;
+	for (it = _articles.begin(); it != _articles.end(); it++)
+	{
+		if (it->GetHeadLine() == articleToDelete.GetHeadLine())
+		{
+			idToDelete = it->GetID();
+			_articles.erase(it);
+		}
+	}
+	map <int, int> ::iterator it1;
+	it1 = _popularity.find(idToDelete);
+	_popularity.erase(it1);
+}
