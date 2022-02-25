b2670e4f20655250569fd626cbbcacae08366f3a
FloodDis <vlad.sechenov@mail.ru>
2022-02-26 02:01:10 +0700

    Написал метод AddArticle класса NewsSystem

--- Новостная система(Стажировка)/NewsSystem.cpp
+++ Новостная система(Стажировка)/NewsSystem.cpp
@@ -4,3 +4,8 @@ Article NewsSystem::GetRecommendation(string user)
 {
 	return _recommendationSystem.GetRecommendation(user, _articles);
 }
+
+void NewsSystem::AddArticle(Article newArticle)
+{
+	_articles.push_back(newArticle);
+}
