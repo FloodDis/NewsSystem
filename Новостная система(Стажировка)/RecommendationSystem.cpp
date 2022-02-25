f038bae6359d7d640cf4589a1b4c4617384565dc
FloodDis <vlad.sechenov@mail.ru>
2022-02-25 22:09:56 +0700

    Дописал реализацию метода FixReading класса RecommendationSystem

--- Новостная система(Стажировка)/RecommendationSystem.cpp
+++ Новостная система(Стажировка)/RecommendationSystem.cpp
@@ -2,9 +2,18 @@
 
 void RecommendationSystem::FixReading(string user, Article newArticle)
 {
-	list<Article> newList;
-	newList.push_back(newArticle);
-	_readList.insert(user, newList);
+	map <string, list<Article>> ::iterator it;
+	it = _readList.find(user);
+	if (it == _readList.end())
+	{
+		list<Article> newList;
+		newList.push_back(newArticle);
+		_readList.insert({ user, newList });
+	}
+	else
+	{
+		it->second.push_back(newArticle);
+	}
 }
 
 Article RecommendationSystem::GetRecommendation(string user, list<Article> avaibleArticles)
