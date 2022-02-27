e6b8fd36b9cfad7551e36311a5a4babbeb2b1c49
FloodDis <vlad.sechenov@mail.ru>
2022-02-27 19:37:32 +0700

    Создал сервер SServer

---
+++ Новостная система(Стажировка)/Main.cpp
@@ -0,0 +1,11 @@
+#include "SServer.h"
+#include "NewsSystem.h"
+
+int main()
+{
+	SServer server;
+	server.port = 3487;//порт это ushort - так что cin.get() и т.п. 
+	//функции тут работаю некорректно. Лучше задать фвручную
+	server.startServer();
+	return 0;
+}
